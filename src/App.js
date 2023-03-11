e2313a3bfab6a211e9c30bde8719038b3641111c
seop3 <dlgustjqw@naver.com>
2023-03-12 01:47:35 +0900

    Navbar 작업중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+// import { IconStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar, faLanguage, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <Navbar></Navbar>
+      {/* TODO: 리액트 공식문서처럼 Navbar에 언어변경이랑 깃허브 추가하기, 반응형 햄버거 아이콘 추가하기
+       // CSS 글로벌 코드 작성하기 // 마크업 끝내기 */}
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div id='nav'>
+      <div className='nav__title'>
+        <FontAwesomeIcon icon={faStar} size='1x' />
+        <a href='#top'>Portfolio</a>
+      </div>
+      <ul className='nav__ul'>
+        <li>Home</li>
+        <li>About me</li>
+        <li>Skills</li>
+        <li>My goals</li>
+        <li>Review</li>
+        <li>Contact</li>
+      </ul>
+      <div className='nav__right'>
+        <div>
+          <FontAwesomeIcon icon={faLanguage} size='2x'/>
+          <span>Languages</span>
+        </div>
+        <a href='#top'>
+          <span>GitHub</span>
+          <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='1x' className='nav__github'/>
+          <FontAwesomeIcon icon={faGithub} size='2x' className='nav__github'/>
         </a>
-      </header>
+      </div>
+      
     </div>
   );
 }
