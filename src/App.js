5de9e49cd5576cf48055784827758aad3988f5d6
seop3 <dlgustjqw@naver.com>
2023-03-12 22:24:24 +0900

    Navbar 완성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import './App.css';
-// import { IconStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faStar, faLanguage, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faLanguage, faArrowUpRightFromSquare, faBars } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { useState, useEffect } from 'react';
+
 
 
 
 function App() {
+
   return (
     <div>
       <Navbar></Navbar>
-      {/* TODO: 리액트 공식문서처럼 Navbar에 언어변경이랑 깃허브 추가하기, 반응형 햄버거 아이콘 추가하기
-       // CSS 글로벌 코드 작성하기 // 마크업 끝내기 */}
+      {/* TODO: 
+       // CSS 글로벌 코드 작성하기
+       // Navbar 컴포넌트 따로 파일로 뺴서 import/export하기
+       // Redux로 스테이트 관리하기 */}
+
     </div>
   );
 }
 
+
+
 function Navbar() {
+
+  const [toggle, setToggle] = useState(true);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1024) {
+        const navLi = document.querySelectorAll('.nav__li');
+        navLi.forEach((li) => {
+          li.style.display = 'block';
+        });
+      } else {
+        const navLi = document.querySelectorAll('.nav__li');
+        navLi.forEach((li) => {
+          li.style.display = toggle ? 'block' : 'none';
+        });
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [toggle]);
+
   return (
     <div id='nav'>
       <div className='nav__title'>
@@ -24,14 +56,17 @@ function Navbar() {
         <a href='#top'>Portfolio</a>
       </div>
       <ul className='nav__ul'>
-        <li>Home</li>
-        <li>About me</li>
-        <li>Skills</li>
-        <li>My goals</li>
-        <li>Review</li>
-        <li>Contact</li>
+        <li style={{display:toggle ? 'block' : 'none'}} className='nav__li'>Home</li>
+        <li style={{display:toggle ? 'block' : 'none'}} className='nav__li'>About me</li>
+        <li style={{display:toggle ? 'block' : 'none'}} className='nav__li'>Skills</li>
+        <li style={{display:toggle ? 'block' : 'none'}} className='nav__li'>My goals</li>
+        <li style={{display:toggle ? 'block' : 'none'}} className='nav__li'>Review</li>
+        <li style={{display:toggle ? 'block' : 'none'}} className='nav__li'>Contact</li>
       </ul>
       <div className='nav__right'>
+        <button className='nav__toggle' onClick={handleToggle}>
+          <FontAwesomeIcon icon={faBars} size='2x'/>
+        </button>
         <div>
           <FontAwesomeIcon icon={faLanguage} size='2x'/>
           <span>Languages</span>
@@ -42,7 +77,6 @@ function Navbar() {
           <FontAwesomeIcon icon={faGithub} size='2x' className='nav__github'/>
         </a>
       </div>
-      
     </div>
   );
 }
